45835032d313c2ab0bc20963a4d5f2c5bf3d607c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-02-19 09:14:32 -0500

    spelling: infinite
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/controller/certificates/revisionmanager/fuzz_test.go
+++ pkg/controller/certificates/revisionmanager/fuzz_test.go
@@ -34,7 +34,7 @@ import (
 // overapproximates which can result in false positives.
 // The fuzzer does not verify how Cert-Manager behaves. It tests for panics
 // or unrecoverable issues such as stack overflows, excessive memory usage,
-// deadlocks, inifinite loops and other similar issues.
+// deadlocks, infinite loops and other similar issues.
 func FuzzProcessItem(f *testing.F) {
 	f.Fuzz(func(t *testing.T,
 		data []byte,
